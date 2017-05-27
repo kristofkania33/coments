790bbcb03963f74d3c0b82f79f24865ddad7ec3f
kristofkania33 <zyhkana25@wp.pl>
2017-05-27 14:43:35 +0200

    coments

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//scripts.js
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+// Start working on console
+
+/* 
+	Introduction to homepage.
+	In future I add new js.
+*/
